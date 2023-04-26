bef18d5a058eaa50a461d293fbca3aac389da73a
Sandun Rathsara <sandun@surge.global>
2023-04-26 12:44:12 +0530

    Nav bar completed

--- src/components/AppContainer/index.tsx
+++ src/components/AppContainer/index.tsx
@@ -1,9 +1,10 @@
-import { Layout, Menu } from 'antd'
+import { FC, useMemo } from 'react'
+import { Avatar, Layout, Menu, Typography } from 'antd'
 import { Outlet, useNavigate, useLocation } from 'react-router-dom'
 import { MenuItemType } from 'antd/es/menu/hooks/useItems'
-import { FC, useMemo } from 'react'
 import { APP_ROUTES } from '../../configs/constants/APP_ROUTES'
-import styles from './app-container.module.css'
+import styles from './index.module.css'
+import Clock from '../Clock'
 
 const AppContainer: FC = () => {
   const navigate = useNavigate()
@@ -19,8 +20,15 @@ const AppContainer: FC = () => {
 
   return (
     <Layout className={styles.app}>
-      <Layout.Header>
+      <Layout.Header className={styles.menu}>
         <Menu theme={'dark'} mode="horizontal" defaultSelectedKeys={[pathname.slice(1) || 'home']} items={navItems} />
+        <div className={styles.user}>
+          <Clock className={styles.user_name} />
+          <Avatar
+            src={`https://avatars.dicebear.com/api/micah/JohnMorrison.svg?radius=10&size=40&backgroundColor=white&scale=80`}
+          />
+          <Typography.Text className={styles.user_name}>John Morrison</Typography.Text>
+        </div>
       </Layout.Header>
       <Layout.Content className={styles.container}>
         <Outlet />
--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,11 +1,7 @@
-import { Link } from 'react-router-dom'
-import { APP_ROUTES } from '../../configs/constants/APP_ROUTES'
-
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
-      <Link to={APP_ROUTES.FAVOURITES}>Favourites</Link>
     </div>
   )
 }
