192df0d745ac47e5a8d6be78c72db11070b7e425
Sandun Rathsara <sandun@surge.global>
2023-04-26 16:32:27 +0530

    Error handled on cocktails fetching

--- src/features/Home/fetcher.ts
+++ src/features/Home/fetcher.ts
@@ -8,13 +8,16 @@ export const fetchRandomCocktails = (numberOfItems: number) => async (url: strin
   const cocktails: TCocktail[] = await Promise.all(
     new Array(numberOfItems).fill(1).map(async () => {
       const data = await fetch(url)
-        .then(res => res.json())
-        .then(res => res.drinks[0])
+
+      if (!data.ok) throw new Error('Error fetching cocktails')
+
+      const cocktails = await data.json().then(res => res.drinks[0])
+
       return {
-        title: data.strDrink,
-        category: data.strCategory,
-        id: data.idDrink,
-        image: data.strDrinkThumb,
+        title: cocktails.strDrink,
+        category: cocktails.strCategory,
+        id: cocktails.idDrink,
+        image: cocktails.strDrinkThumb,
       } as TCocktail
     })
   )
--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,29 +1,60 @@
 import CocktailCard from '../../components/CocktailCard'
-import { Button, Col, Row } from 'antd'
+import { Button, Col, Row, Input, Spin, notification } from 'antd'
 import { HeartOutlined, PlusOutlined } from '@ant-design/icons'
 import { useHomeReducer } from './reducer'
+import styles from './index.module.css'
+import { useEffect } from 'react'
 
 const Home = () => {
   const { state } = useHomeReducer()
 
+  const [api, contextHolder] = notification.useNotification()
+
+  useEffect(() => {
+    if (state.cocktailFetchingError)
+      api.info({
+        message: 'Failed to fetch cocktails',
+        placement: 'topRight',
+      })
+  }, [state.cocktailFetchingError, api])
+
   return (
     <div>
+      {contextHolder}
+      <Row gutter={[16, 16]}>
+        <Col span={6}>
+          <Input.Search
+            placeholder="Search cocktails"
+            onSearch={value => console.log('value', value)}
+            style={{ width: 200 }}
+          />
+        </Col>
+        <Col span={6} offset={12}>
+          <h2>refresh</h2>
+        </Col>
+      </Row>
       <Row justify={'space-evenly'} gutter={[16, 16]} wrap>
-        {state?.cocktails?.map(cocktail => (
-          <Col key={cocktail.image}>
-            <CocktailCard
-              key={cocktail.image}
-              image={cocktail.image}
-              name={cocktail.title}
-              category={cocktail.category}
-              actions={[
-                <Button type={'text'} icon={<PlusOutlined />}>
-                  Add to <HeartOutlined />
-                </Button>,
-              ]}
-            />
-          </Col>
-        ))}
+        {state.isCocktailFetching ? (
+          <div className={styles.loading_container}>
+            <Spin />
+          </div>
+        ) : (
+          state?.cocktails?.map(cocktail => (
+            <Col key={cocktail.image}>
+              <CocktailCard
+                key={cocktail.image}
+                image={cocktail.image}
+                name={cocktail.title}
+                category={cocktail.category}
+                actions={[
+                  <Button type={'text'} icon={<PlusOutlined />}>
+                    Add to <HeartOutlined />
+                  </Button>,
+                ]}
+              />
+            </Col>
+          ))
+        )}
       </Row>
     </div>
   )
